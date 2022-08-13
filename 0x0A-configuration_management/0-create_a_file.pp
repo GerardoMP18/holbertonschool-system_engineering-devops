98218032d07bf61419abadda14f986b94fd3c918
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-12 19:13:27 -0700

    fix: added to the ensure file attribute

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,11 +1,12 @@
 # Create a file
-# ensure ---> present = create new file if not exits the file
+# ensure ---> file    = ensures it’s a normal file, and enables use
 # mode   ---> 0744    = the desired permission mode for the file
 # owner  --->         = The user to whom the file should belong
 # group  --->         = Which group should own the file
 # content -->         = The desired contens of a file
+
 file { '/tmp/school':
-  ensure  => '1.1.0',
+  ensure  => 'file',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
