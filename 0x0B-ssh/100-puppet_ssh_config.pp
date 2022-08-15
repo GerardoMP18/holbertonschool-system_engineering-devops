ce2430917a2eaad79b348391d3103d8833a9b6df
gerardomp18 <gerardomarinparra18@gmail.com>
2022-08-14 21:14:22 -0500

    feat: configuration file with puppets

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# configuration of file with puppets
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '#   PasswordAuthentication no',
+  match  => '#   PasswordAuthentication yes',
+}
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '#   IdentityFile ~/.ssh/school',
+  match  => '#   IdentityFile ~/.ssh/id_rsa',
+}
