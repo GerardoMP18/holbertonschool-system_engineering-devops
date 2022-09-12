d917c32ff76ac977cb828b38fd00046108d1e54b
gerardomp18 <gerardomarinparra18@gmail.com>
2022-09-12 18:18:06 -0500

    [feat] Adding script fot the export en file jspn

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" script to export data in the CSV format """
+
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    endpointUser = "https://jsonplaceholder.typicode.com/users/{}".format(
+            sys.argv[1])
+
+    responseUser = requests.get(endpointUser)
+    USERNAME = responseUser.json().get("username")
+
+    endpTodos = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+            sys.argv[1])
+
+    responseTodos = requests.get(endpTodos)
+    task = responseTodos.json()
+
+    newList = []
+    for tasks in task:
+        newdict = {}
+        newdict['task'] = tasks.get("title")
+        newdict['completed'] = tasks.get("completed")
+        newdict['username'] = USERNAME
+        newList.append(newdict)
+
+    listdataID = {sys.argv[1]: newList}
+
+    with open('{}.json'.format(sys.argv[1]), 'w') as json_file:
+        json.dump(listdataID, json_file)
