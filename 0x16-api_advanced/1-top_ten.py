ddc4311d6003a08d466ed1d5555ed4066fbf08c4
gerardomp18 <gerardomarinparra18@gmail.com>
2022-09-27 18:02:13 -0500

    [feat] Adding function to show the first 10post of the reddit api

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Top Ten """
+
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    script to show the first 10 posts of the reddit api
+    """
+
+    endpoint = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    user = {'User-Agent': 'gerardomp18'}
+    response = requests.get(endpoint, headers=user,
+                            allow_redirects=False,
+                            params={'limit': '10'})
+
+    if response.status_code == 200:
+        listPost = response.json().get('data').get('children')
+        for post in listPost:
+            print(post.get('data').get('title'))
+    return 0
